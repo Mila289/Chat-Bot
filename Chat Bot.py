82e10e7371f22b4afc930dac6e9941abaf2afaed
Mila289 <mila.krasulina@gmail.com>
2025-01-04 12:59:22 +0700

    Устанавливаю необходимые библиотеки.

---
+++ Chat Bot.py
@@ -0,0 +1,5 @@
+#1)pip install ChatterBot chatterbot-corpus spacy
+#2) python3 -m spacy download en_core_web_sm Or... choose the language you prefer
+#3) Navigate to your Python3 directory
+#4) Modify Lib/site-packages/chatterbot/tagging.py to properly load 'en_core_web_sm'
+
